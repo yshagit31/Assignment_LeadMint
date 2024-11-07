908771da27f6131a5837a8194b515541e4f9096d
yshagit31 <yshashank024@gmail.com>
2024-11-08 01:42:45 +0530

    Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './App.css'
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './components/Register'
 import Login from './components/Login'
